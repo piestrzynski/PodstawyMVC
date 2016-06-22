7be29f10e9b06780493e81abeb5f94ce44764f5d
KapitanTochimo <misiek092@interia.pl>
2016-06-22 13:37:48 +0200

    Wstep do MVC czysty kod

---
+++ MVCPodstawy/MVCPodstawy/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCPodstawy.Models;
+namespace MVCPodstawy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var lista = new List<Alb>
+            {
+            new Alb {Nazwa="Pierwsza",Zawartosc="Pierwsza Zawartosc"},
+            new Alb {Nazwa="Druga ",Zawartosc="Druga Zawartosc" },
+            new Alb {Nazwa="Trzecia", Zawartosc="Trzecia Zawartosc" }
+            };
+
+            ViewBag.AL = lista;
+
+
+            return View();
+        }
+
+        //public string Index(string id)
+        //{
+        //    return "Witaj, " + id;
+        //}
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            //ViewBag.Test = 12;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCPodstawy/MVCPodstawy/Models/Alb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCPodstawy.Models
+{
+    public class Alb
+    {
+        public string Nazwa { get; set; }
+        public string Zawartosc { get; set; }
+
+}
+}
